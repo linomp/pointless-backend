8c249e8bccab9ac44e26f4a056fc219aa3b077bb
linomp <lm2p@hotmail.es>
2023-01-29 12:23:19 +0100

    configure CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
+import os
+
 from fastapi import FastAPI
 
-from app.routers import healthcheck, experiments
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.routers import healthcheck, experiments
 app = FastAPI(title="pointless-backend", description="Backend for my Pointless personal site")
 
 app.include_router(healthcheck.router)
 app.include_router(experiments.router)
+
+origins = [
+    "https://pointless.xmp.systems",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", tags=["healthcheck"])
